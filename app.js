d824358e7bfd1b49cf96cfa19ccff649dcb1f2eb
raviloi <ravi_dreamz81@yahoo.co.in>
2023-11-19 01:54:09 +0530

    change callback

--- app.js
+++ app.js
@@ -69,8 +69,7 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  // callbackURL: "http://localhost:3000/auth/google/secrets"
-  callbackURL: "https://submit-your-secrets-anonymously.onrender.com"
+  callbackURL: "http://localhost:3000/auth/google/secrets"
 },
 function(accessToken, refreshToken, profile, cb) {
   User.findOrCreate({ googleId: profile.id }, function (err, user) {
