fbf18241a64bb340dd9fec4bdf3f28267d908f78
raviloi <ravi_dreamz81@yahoo.co.in>
2023-11-18 23:39:23 +0530

    modify mongo ip

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(passport.session());
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+  await mongoose.connect('mongodb://0.0.0.0:27017/userDB');
 }
 
 const userSchema = new mongoose.Schema ({
